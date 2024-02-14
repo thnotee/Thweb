1222af127710c0895261db9e030d37816f7014b5
thlee5842@gmail.com <thlee5842@gmail.com>
2024-02-15 06:10:31 +0900

    상품리스트 출력

--- Thweb.Mall/Areas/Customer/Controllers/ProductController.cs
+++ Thweb.Mall/Areas/Customer/Controllers/ProductController.cs
@@ -1,14 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+using Thweb.Data.Repository.IRepository;
+using Thweb.Model.Model;
 
 namespace Thweb.Mall.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class ProductController : Controller
     {
-        
-        public IActionResult Index()
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
         {
-            return View();
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            Expression<Func<Product, DateTime>> orderByEx = x => x.RegDate;
+            var productList = await _unitOfWork.Product.GetPagedListAsync<DateTime>(
+                page: page
+                , pageSize: 10
+                , filter: null
+                , orderBy: orderByEx
+                , descending: false
+            , includeProperties: "Category");
+
+
+            foreach (var item in productList)
+            {
+                Expression<Func<Image, bool>> tableName = x => (x.TableName == "Product" && x.TableId == item.Id);
+                item.Images = await _unitOfWork.Image.GetAllAsync(tableName);
+            }
+
+            return View(productList);
         }
 
         public IActionResult Detail()
